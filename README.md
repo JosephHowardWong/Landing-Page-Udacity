0e9ecc23aa186b576e37883eb5a00a6a90df5349
Joseph Wong <josephwong@Josephs-MacBook-Pro.local>
2021-01-29 14:55:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# THIS IS UDACITY PROJECT NUMBER 2
+
+## Create a Landing Page
+
+**Assume sections are being loaded in from a 3rd party API/CDN _and we don't know ahead of time how much content(sections) will arrive_ successfully**
+
+### The requirements are as follows:
+
+- Build out NavBar _dynamically_ according to which sections we received
+
+- Implement smooth scrolling behaviour to section when nav links are clicked instead of jumping straight to the section
+
+- **make navbar list item active when user scrolls over the corresponding section**
+
+- make >= 4 sections
+
+- make section active when user scrolls over section and deactivate when sections leave current viewport threshold
+
+- Ensure navbar sticks to the top of page _when_ scrolling down the page and then disappears _after_ user has stopped scrolling. Navbar should be present on page load.
+
+- make sections collapsible
+
+- Implement a button that scrolls to the top of page
+
+[check out this devEd video on Intersection Observer](https://www.youtube.com/watch?v=RLc8NB2JyxE)
+
+[check out this MDN doc on Intersection Observer](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API)
+
+## Here's an example of intersection observer from MDN
+
+```javascript
+let options = {
+  root: document.querySelector('#scrollArea'),
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(callback, options);
+```
+
+```javascript
+let target = document.querySelector('#listItem');
+observer.observe(target);
+
+// the callback we setup for the observer will be executed now for the first time
+// it waits until we assign a target to our observer (even if the target is currently not visible)
+```
+
+```javascript
+let callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    // Each entry describes an intersection change for one observed
+    // target element:
+    //   entry.boundingClientRect
+    //   entry.intersectionRatio
+    //   entry.intersectionRect
+    //   entry.isIntersecting
+    //   entry.rootBounds
+    //   entry.target
+    //   entry.time
+  });
+};
+```
+
+### Intersection Observer diagram from https://blog.jxck.io/entries/2016-06-25/intersection-observer.svg
+
+![Intersection Observer diagram from https://blog.jxck.io/entries/2016-06-25/intersection-observer.svg](https://blog.jxck.io/entries/2016-06-25/intersection-observer.svg)
+
+### MDN said the following about Intersection Observer:
+
+> Implementing intersection detection in the past involved event handlers and loops calling methods like Element.getBoundingClientRect() to build up the needed
+> information for every element affected. Since all this code runs on the main thread, even one of these can cause performance problems. When a site is loaded with
+> these tests, things can get downright ugly.
---
+++ app.js
@@ -0,0 +1,124 @@
+// GET ALL NEEDED DOM ELEMENTS
+const navBar = document.querySelector('#navbar__list');
+
+const topBtn = document.querySelector('.top-btn');
+
+const collapseBtn = document.querySelectorAll('.collapse');
+
+const sections = document.querySelectorAll('section');
+
+const docFrag = document.createDocumentFragment();
+
+
+//BUILD OUT UNORDERED LIST OF NAV ITEMS
+sections.forEach(section => {
+    const navLi = document.createElement('li');
+    const anch = document.createElement('a');
+    anch.innerHTML = section.getAttribute('data-nav');
+    anch.classList.add(section.id);
+    anch.href = `#${section.id}`;
+    navLi.appendChild(anch);
+    docFrag.appendChild(navLi);
+})
+
+navBar.appendChild(docFrag);
+
+
+// USE INTERSECTION OBSERVER TO ACTIVATE THE NAV ANCH AND SECTION IN VIEW
+const options = {
+    threshold: 0.6
+}
+
+let observer = new IntersectionObserver(activeSec, options);
+
+function activeSec(entries) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("your-active-class");
+            activateSecNav(entry.target.id);
+        }
+        else {
+            entry.target.classList.remove("your-active-class");
+        }
+    });
+}
+
+sections.forEach(section => {
+    observer.observe(section);
+})
+
+
+// ACTIVATE ANCH CORRESPONDING TO SECTION IN VIEW AND TOGGLE "GO TO TOP BUTTON"
+const activateSecNav = anchId => {
+    const anchList = document.querySelectorAll('a');
+    anchList.forEach( anch => {
+        if(anch.classList.contains(anchId)) {
+            anch.classList.add("your-active-class");
+        }
+        else {
+            anch.classList.remove("your-active-class");
+        }
+        if(anch.classList.contains("your-active-class") && !anch.classList.contains("home")) {
+            topBtn.classList.add("your-active-class");
+        }
+        else if(anch.classList.contains("your-active-class") && anch.classList.contains("home")) {
+            topBtn.classList.remove("your-active-class");
+        }
+    })
+}
+
+
+// ON ANCHOR CLICK, SMOOTH SCROLL TO SECTION
+const anchors = document.querySelectorAll('#navbar__list a');
+
+anchors.forEach(anchor=> {
+    anchor.addEventListener('click', 
+        (e) => scrollToSec(e, anchor.classList[0]))
+});
+
+const scrollToSec = (e, anchId) => {
+    e.preventDefault();
+    const sec = document.querySelector(`#${anchId}`);
+    sec.scrollIntoView({block: "end", behavior: "smooth"});
+}
+
+
+// SCROLL TO TOP BUTTON
+const pgHead = document.querySelector('.page__header');
+
+topBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    pgHead.scrollIntoView({behavior: "smooth"});
+})
+
+
+// HIDE NAVBAR AFTER SCROLLING STOP (SHOULD BE VISIBLE AT TOP)
+const nav = document.querySelector('nav');
+
+const homeSec = document.querySelector('#home');
+
+window.onscroll = () => {
+    const coords = homeSec.getBoundingClientRect();
+    if(coords.bottom < 500) {
+        nav.classList.remove('no-show');
+        setTimeout(() => nav.classList.add('no-show'), 2000);
+    }
+    if(coords.bottom >= 500) {
+        setTimeout(() => nav.classList.remove('no-show'), 2000);
+    }
+}
+
+
+// MAKE SECTIONS COLLAPSIBLE
+const toggleSec = (e) => {
+    e.preventDefault();
+    sections.forEach(sec => {
+        if(e.target.classList.contains(sec.id)) {
+            sec.classList.toggle("sec-collapse");
+        }
+    })
+}
+
+collapseBtn.forEach( Btn => {
+    Btn.addEventListener('click', toggleSec);
+})
